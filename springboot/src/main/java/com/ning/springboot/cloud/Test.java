9610a455e155173a8de3033fdaefae1e7bb72705
“yourkrismile1 <gninna@163.com>
2019-06-27 16:09:23 +0800

    添加测试

---
+++ springboot/src/main/java/com/ning/springboot/cloud/Test.java
@@ -0,0 +1,13 @@
+package com.ning.springboot.cloud;
+
+/**
+ * @Author: an
+ * @Date: 2019/6/27
+ * @Description:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
